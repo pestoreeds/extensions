a8314ac31b3c0db03002c683f81f29abfd5c3d95
ryanmart00 <ryanmart00@gmail.com>
2025-01-06 12:57:03 -0800

    debugging time

--- src/eHentai/eHentaiParser.ts
+++ src/eHentai/eHentaiParser.ts
@@ -87,6 +87,7 @@ export async function parsePages(id: string, pageCount: number, requestManager: 
     for (let i = 0; i <= pageCount / 20; i++) {
         pageArr.push(parsePage(id, i, requestManager, cheerio))
     }
+    throw new Error(pageArr.length)
 
     return Promise.all(pageArr).then(pages => pages.reduce((prev, cur) => [...prev, ...cur], []))
 }
