730a98cef48a73c5757132f440b45897f0008296
pestoreeds <ryanmetroid@gmail.com>
2025-01-07 02:26:35 +0000

    Update gh-pages to output generated at c4162e6

--- eHentai/source.js
+++ eHentai/source.js
@@ -590,8 +590,7 @@ async function getSearchData(query, page, categories, requestManager, cheerio, s
     }
     const json = mangaIds.length != 0 ? await getGalleryData(mangaIds, requestManager) : [];
     const results = [];
-    const next = $('a', '#unext').attr('href') ?? 'jef';
-    throw new Error(next);
+    const next = $('#unext').attr('href') ?? '';
     for (const entry of json) {
         results.push(createMangaTile({
             id: next,
